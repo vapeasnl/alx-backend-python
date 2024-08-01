54a6c8705283339bed428acac0f82eabdc19528b
abdessamadsalim <samimalim1991@gmail.com>
2024-08-01 03:47:28 -0700

    upd files

--- 0x00-python_variable_annotations/101-main.py
+++ 0x00-python_variable_annotations/101-main.py
@@ -3,4 +3,4 @@ safely_get_value = __import__('101-safely_get_value').safely_get_value
 annotations = safely_get_value.__annotations__
 print("Here's what the mappings should look like")
 for k, v in annotations.items():
-print( ("{}: {}".format(k, v)))
+    print( ("{}: {}".format(k, v)))
--- 0x00-python_variable_annotations/5-main.py
+++ 0x00-python_variable_annotations/5-main.py
@@ -4,5 +4,4 @@ floats = [3.14, 1.11, 2.22]
 floats_sum = sum_list(floats)
 print(floats_sum == sum(floats))
 print(sum_list.__annotations__)
-print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_
-    sum)))
+print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
