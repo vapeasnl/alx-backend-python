02699f67e7a9aeddeecad81f2f2769c0fb48227a
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-08-01 12:36:51 +0200

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-Module for type-annotated function floor which takes a float n as argument and returns the floor of the float.
+Module for type-annotated function floor
 """
 import math
 
 def floor(n: float) -> int:
     """
-    Returns the floor of the given float.
+    Returns the floor
     """
     return math.floor(n)
